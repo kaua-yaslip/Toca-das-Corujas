1831b33646a078d5d0cfc9637e3b1c383934d7a1
kaua-yaslip <kaua.yaslip@gmail.com>
2026-08-17 11:09:15 -0300

    feat: adicionando head nova

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import Topo from "@/partials/Topo";
 import Rodape from "@/partials/Rodape";
 import RodapeMobile from "@/partials/RodapeMobile";
@@ -12,6 +13,9 @@ export const metadata: Metadata = {
     template: "%s | Toca das Corujas",
   },
   description: "Toca das Corujas, hotel fazenda em Angatuba, São Paulo.",
+  verification: {
+    google: "YIY17hdR-i_UsdW-LNMkEogg8ZvYNDkjkgpor8YcVZM",
+  },
   icons: {
     icon: [
       {
@@ -36,6 +40,27 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="pt-BR">
       <body>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NJB32JXZ');`}
+        </Script>
+        {/* End Google Tag Manager */}
+
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NJB32JXZ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <Topo />
         {children}
         <Rodape />
