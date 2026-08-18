d6467b155d97e5f2bb1e820fc7fc7c6ea17a7682
kaua-yaslip <kaua.yaslip@gmail.com>
2026-08-18 09:07:13 -0300

    feat: atualização de redirects

--- next.config.ts
+++ next.config.ts
@@ -8,135 +8,64 @@ const nextConfig: NextConfig = {
       // REDIRECTS DO SITE ANTIGO PHP
       // ==================================================
 
-      // Adventure
       {
-        source: "/adventure.php",
-        destination: "/lazer",
+        source: "/animais-cavalos-montados.php",
+        destination: "/passeio-a-cavalo",
         statusCode: 301,
       },
-
-      // Campo de futebol
       {
-        source: "/campo-futebol-society.php",
-        destination: "/campo-de-futebol",
+        source: "/cavalo-garanhao-apache.php",
+        destination: "/passeio-a-cavalo",
         statusCode: 301,
       },
-
-      // Cavalos e pôneis adestrados
       {
-        source: "/cavalos-poneis-adestrados.php",
-        destination: "/hotel-fazenda-com-passeio-de-cavalos-e-poneis-adestrados",
+        source: "/cavalo-garanhao-flape.php",
+        destination: "/passeio-a-cavalo",
         statusCode: 301,
       },
-
-      // Contato
       {
-        source: "/contato.php",
-        destination: "/contato",
-        statusCode: 301,
-      },
-
-      // Rapel
-      {
-        source: "/descida-rapel.php",
-        destination: "/lazer",
-        statusCode: 301,
-      },
-
-      // Lazer
-      {
-        source: "/lazer.php",
-        destination: "/lazer",
-        statusCode: 301,
-      },
-
-      // Localização
-      {
-        source: "/localizacao.php",
-        destination: "/contato",
-        statusCode: 301,
-      },
-
-      // Mapa do site
-      {
-        source: "/mapa-site.php",
-        destination: "/mapa-site",
-        statusCode: 301,
-      },
-
-      // Motel fazenda
-      {
-        source: "/motel-fazenda.php",
-        destination: "/hotel-fazenda",
-        statusCode: 301,
-      },
-
-      // Passeio ecológico
-      {
-        source: "/passeio-ecologico.php",
-        destination: "/hotel-fazenda-com-trilha",
-        statusCode: 301,
-      },
-
-      // Passeios com cavalos e pôneis
-      {
-        source: "/passeios-fotos-cavalos-poneis.php",
+        source: "/cavalos-animais-toca-das-corujas.php",
         destination: "/hotel-fazenda-com-passeio-de-cavalos-e-poneis",
         statusCode: 301,
       },
-
-      // Pesca esportiva
       {
-        source: "/pesca-esportiva.php",
-        destination: "/hotel-fazenda-com-pesca-esportiva",
+        source: "/cavalos-garanhao.php",
+        destination: "/passeio-a-cavalo",
         statusCode: 301,
       },
-
-      // Piscina
       {
-        source: "/piscina-com-cascata-trampolim.php",
-        destination: "/piscina",
+        source: "/cavalos-potros.php",
+        destination: "/hotel-fazenda-com-passeio-de-cavalos-e-poneis",
         statusCode: 301,
       },
-
-      // Próxima festa
       {
-        source: "/proxima-festa.php",
+        source: "/egua-matrizes-lusitana.php",
+        destination: "/passeio-a-cavalo",
+        statusCode: 301,
+      },
+      {
+        source: "/egua-matriz-tulipa.php",
+        destination: "/passeio-a-cavalo",
+        statusCode: 301,
+      },
+      {
+        source: "/esportes-nauticos.php",
+        destination: "/passeios-nauticos",
+        statusCode: 301,
+      },
+      {
+        source: "/potro-lusitano-beaujolais.php",
+        destination: "/passeio-a-cavalo",
+        statusCode: 301,
+      },
+      {
+        source: "/salao-festas.php",
         destination: "/salao-para-festas",
         statusCode: 301,
       },
-
-      // Sauna
       {
-        source: "/sauna-seca-vapor.php",
+        source: "/sauna-vapor-1.php",
         destination: "/saunas-seca-e-a-vapor",
-        statusCode: 301,
-      },
-
-      // Suítes
-      {
-        source: "/suites.php",
-        destination: "/suites",
-        statusCode: 301,
-      },
-
-      // Última festa
-      {
-        source: "/ultima-festa.php",
-        destination: "/salao-para-festas",
-        statusCode: 301,
-      },
-
-      // Venda de cavalos e pôneis
-      {
-        source: "/venda-cavalos-poneis-adestrados.php",
-        destination: "/hotel-fazenda-com-passeio-de-cavalos-e-poneis-adestrados",
-        statusCode: 301,
-      },
-
-      {
-        source: "/festas-sitio.php",
-        destination: "/salao-para-festas",
         statusCode: 301,
       },
 
