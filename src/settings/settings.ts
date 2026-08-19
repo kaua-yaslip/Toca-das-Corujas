64f97db57cff4ffe9c16703f7846c2857f066b07
kaua-yaslip <kaua.yaslip@gmail.com>
2026-08-19 09:25:54 -0300

    feat: ajustes pedido pelo felps

--- src/settings/settings.ts
+++ src/settings/settings.ts
@@ -26,8 +26,8 @@ export const settings: SiteSettings = {
   numeroTelefone: "99564-5738",
   whatsappApi: "https://api.whatsapp.com/send?phone=5511995645738&text=Olá!%20Vim%20do%20Google%20e%20gostaria%20de%20mais%20informações%20e%20Orçamento",
   numeroWhatsapp: "99564-5738",
-  email: "marcos@yaslip.com.br",
-  emailDestinatario: "marcos@yaslip.com.br",
+  email: "hoteltocadascorujas@gmail.com",
+  emailDestinatario: "hoteltocadascorujas@gmail.com",
   endereco: {
     // Localização oficial enviada para a Toca das Corujas.
     urlMaps:
